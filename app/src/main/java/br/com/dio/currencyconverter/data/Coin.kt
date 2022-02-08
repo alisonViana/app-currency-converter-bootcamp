453c3ffd6960c1cf40e418477dffb85169793437
alisonViana <alisonviana.c@gmail.com>
2022-02-08 05:13:10 -0300

    Create enum class Coin

---
+++ app/src/main/java/br/com/dio/currencyconverter/data/Coin.kt
@@ -0,0 +1,26 @@
+package br.com.dio.currencyconverter.data
+
+enum class Coin(name: String) {
+    BTC("Bitcoin"),
+    BOB("Boliviano"),
+    VEF("Bolívar Venezuelano"),
+    USD("Dólar Americano"),
+    AUD("Dólar Australiano"),
+    CAD("Dólar Canadense"),
+    HKD("Dólar de Hong Kong"),
+    ETH("Ethereum"),
+    EUR("Euro"),
+    JPY("Iene Japonês"),
+    GBP("Libra Esterlina"),
+    ARS("Peso Argentino"),
+    CLP("Peso Chileno"),
+    COP("Peso Colombiano"),
+    MXN("Peso Mexicano"),
+    UYU("Peso Uruguaio"),
+    BRL("Real Brasileiro"),
+    RUB("Rublo Russo"),
+    INR("Rúpia Indiana"),
+    PEN("Sol do Peru"),
+    XRP("XRP"),
+    KRW("Won Sul-Coreano")
+}
